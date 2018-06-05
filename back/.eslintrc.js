57d43fde4104b5b9299ebb6ce9dd07c50b2a79c0
Hugo Agbonon <hugo@agbonon.fr>
2018-06-05 15:40:34 +0200

    eslint back setup

---
+++ back/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: ['airbnb-base', 'prettier'],
+  env: {
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+  rules: {
+    'class-methods-use-this': 0,
+    'consistent-return': 0, // Often bothering with early returns
+    'import/no-named-as-default': 0,
+    'import/prefer-default-export': 0,
+    'no-nested-ternary': 0,
+    'no-param-reassign': 0, // Useful for our wotk with db models
+  },
+}

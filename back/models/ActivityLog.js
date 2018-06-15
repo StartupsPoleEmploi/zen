d620e54a50250ba04bbab3b4894d0272b8804ced
Hugo Agbonon <hugo@agbonon.fr>
2018-06-15 11:31:24 +0200

    create a table activity-logs to log user actions

---
+++ back/models/ActivityLog.js
@@ -0,0 +1,52 @@
+const { BelongsToOneRelation } = require('objection')
+const BaseModel = require('./BaseModel')
+
+class ActivityLog extends BaseModel {
+  static get tableName() {
+    return 'activity_logs'
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['action'],
+
+      properties: {
+        id: { type: 'integer' },
+        userId: { type: 'integer' },
+        action: { type: 'string' },
+      },
+    }
+  }
+
+  // This object defines the relations to other models.
+  static get relationMappings() {
+    return {
+      user: {
+        relation: BelongsToOneRelation,
+        modelClass: `${__dirname}/User`,
+        join: {
+          from: 'activity_logs.userId',
+          to: 'Users.id',
+        },
+      },
+    }
+  }
+
+  static get actions() {
+    return {
+      VALIDATE_DECLARATION: 'VALIDATE_DECLARATION',
+      VALIDATE_EMPLOYERS: 'VALIDATE_EMPLOYERS',
+      VALIDATE_FILES: 'VALIDATE_FILES',
+    }
+  }
+
+  $beforeUpdate() {} // overrides base model setting updatedAt
+
+  $beforeInsert() {
+    // overrides base model setting updatedAt
+    this.createdAt = new Date().toISOString()
+  }
+}
+
+module.exports = ActivityLog

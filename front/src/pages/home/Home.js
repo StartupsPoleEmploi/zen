afff6f3ad43174f3e5deffe93444905ec5527f5e
Hugo Agbonon <hugo@agbonon.fr>
2018-06-21 11:15:03 +0200

    Add Landing page user steps

--- front/src/pages/home/Home.js
+++ front/src/pages/home/Home.js
@@ -6,6 +6,12 @@ import styled from 'styled-components'
 import LinkButton from '../../components/Generic/LinkButton'
 import PEConnectLink from '../../components/PEConnect/PEConnectLink'
 import landingBackground from './images/landingBackground.svg'
+import logoPE from './images/logoPE.jpg'
+import step1ToStep2 from './images/step1-to-step2.svg'
+import step1 from './images/step1.jpg'
+import step2ToStep3 from './images/step2-to-step3.svg'
+import step2 from './images/step2.jpg'
+import step3 from './images/step3.jpg'
 
 const StyledHome = styled.div`
   display: flex;
@@ -36,6 +42,36 @@ const HeaderMain = styled.div`
 
 const PEConnectLinkContainer = styled.div`
   flex-shrink: 0;
+`
+
+const StepsContainer = styled.div`
+  margin-top: 7.5rem;
+  text-align: center;
+  width: 80rem;
+`
+
+const Step = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`
+
+const StepImg = styled.img`
+  width: 35rem;
+  height: auto;
+`
+
+const IntermediateImg = styled.img`
+  margin-bottom: -5rem;
+  position: relative;
+`
+
+const FooterImg = styled.img`
+  display: block;
+  margin: 15rem auto 4rem;
+  width: 12rem;
+  height: auto;
 `
 
 export const Home = ({ user }) => (
@@ -81,6 +117,47 @@ export const Home = ({ user }) => (
         {!user && <PEConnectLink useDarkVersion />}
       </PEConnectLinkContainer>
     </Header>
+    <StepsContainer>
+      <Typography variant="title">Seulement 3 étapes !</Typography>
+      <Step>
+        <StepImg src={step1} alt="Étape 1" />
+        <div>
+          <Typography variant="title" gutterBottom>
+            Mise à jour de ma situation
+          </Typography>
+          <Typography style={{ width: '30rem' }}>
+            J’actualise ma situation en fonction de chaque employeur.!
+          </Typography>
+        </div>
+      </Step>
+      <IntermediateImg src={step1ToStep2} alt="" />
+      <Step>
+        <div>
+          <Typography variant="title" gutterBottom>
+            Envoi simple et rapide
+          </Typography>
+          <Typography style={{ width: '30rem' }}>
+            On me dit les documents que je dois envoyer.
+          </Typography>
+        </div>
+        <StepImg src={step2} alt="Étape 2" />
+      </Step>
+      <IntermediateImg src={step2ToStep3} alt="" />
+      <Step>
+        <StepImg src={step3} alt="Étape 3" />
+        <div>
+          <Typography variant="title" gutterBottom>
+            Indemnisation sans erreur
+          </Typography>
+          <Typography style={{ width: '30rem' }}>
+            Je suis payé le bon montant, au bon moment.
+          </Typography>
+        </div>
+      </Step>
+    </StepsContainer>
+    <footer>
+      <FooterImg src={logoPE} alt="" />
+    </footer>
   </StyledHome>
 )
 

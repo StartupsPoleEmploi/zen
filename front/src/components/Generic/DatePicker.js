2d87f51c0258096c8c76b77dde8b43773b3312cc
Hugo Agbonon <hugo@agbonon.fr>
2018-08-01 15:45:24 +0200

    hotfix: the datepicker was not behaving correctly when the active month was not the current month
    bug with autoOK / maxDate set

--- front/src/components/Generic/DatePicker.js
+++ front/src/components/Generic/DatePicker.js
@@ -36,7 +36,7 @@ export default class DatePicker extends PureComponent {
     return (
       <MuiPickersUtilsProvider utils={MomentUtils} moment={moment} locale="fr">
         <StyledMuiDatePicker
-          autoOk
+          cancelLabel="Annuler"
           label={this.props.label}
           format="DD/MM/YYYY"
           onChange={this.handleDateChange}

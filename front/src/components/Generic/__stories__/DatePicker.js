17d3e8374735dc3db854e63ae00890fb1a034ea7
Hugo Agbonon <hugo@agbonon.fr>
2018-08-02 10:51:17 +0200

    fixup! update front stories

--- front/src/components/Generic/__stories__/DatePicker.js
+++ front/src/components/Generic/__stories__/DatePicker.js
@@ -1,14 +1,28 @@
 import { action } from '@storybook/addon-actions'
 import { storiesOf } from '@storybook/react'
+import moment from 'moment'
 import React from 'react'
 import { host } from 'storybook-host'
 
 import DatePicker from '../DatePicker'
 
+const activeMonthMoment = moment('2018-07-01T09:51:28.389Z')
+
+const minDate = activeMonthMoment
+  .clone()
+  .startOf('month')
+  .toDate()
+const maxDate = activeMonthMoment
+  .clone()
+  .endOf('month')
+  .toDate()
+
 const defaultProps = {
   label: 'Entrez une date',
   name: 'date',
   onSelectDate: action('onSelectDate'),
+  maxDate,
+  minDate,
 }
 
 storiesOf('DatePicker', module)
@@ -22,6 +36,9 @@ storiesOf('DatePicker', module)
   .add('with selected date', () => (
     <DatePicker
       {...defaultProps}
-      value={new Date('2018-06-18T09:26:06.026Z')}
+      value={activeMonthMoment
+        .clone()
+        .add(1, 'day')
+        .toDate()}
     />
   ))

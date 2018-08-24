79e5b8f73adf86d10d322aa419be6e87d53b52f5
Hugo Agbonon <hugo@agbonon.fr>
2018-08-24 10:49:43 +0200

    Restyle Thanks page, move images to be used there

--- front/src/pages/actu/Thanks.js
+++ front/src/pages/actu/Thanks.js
@@ -1,8 +1,10 @@
 import Typography from '@material-ui/core/Typography'
 import moment from 'moment'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { Fragment } from 'react'
 import styled from 'styled-components'
+
+import moneyBank from '../../images/money-bank.svg'
 
 const StyledThanks = styled.div`
   margin: auto;
@@ -14,22 +16,57 @@ const StyledThanks = styled.div`
   max-width: 48rem;
 `
 
-const LandingText = styled(Typography)`
-  padding: 8rem 0;
+const StyledImg = styled.img`
+  max-width: 30rem;
+  width: 100%;
 `
 
-export const Thanks = ({ activeMonth, location: { search } }) => (
-  <StyledThanks>
-    <LandingText variant="headline">Merci !</LandingText>
-    <LandingText variant="title">
-      {search.includes('later')
-        ? 'Vous pourrez reprendre ultérieurement'
-        : `Vous avez terminé votre declaration ${
-            activeMonth ? `de ${moment(activeMonth).format('MMMM')} !` : '' // eslint-disable-line no-irregular-whitespace
-          }`}
-    </LandingText>
-  </StyledThanks>
-)
+const Title = styled(Typography)`
+  padding: 4rem 0;
+`
+
+export const Thanks = ({ activeMonth, location: { search } }) => {
+  const isDeclarationFinished = !search.includes('later')
+
+  return (
+    <StyledThanks>
+      <StyledImg src={moneyBank} alt="" />
+      {isDeclarationFinished ? (
+        <Fragment>
+          <Title variant="title">
+            Merci, votre actualisation et l'envoi de vos documents sont terminés
+            {activeMonth
+              ? ` pour le mois de ${moment(activeMonth).format('MMMM')} ! ` // eslint-disable-line no-irregular-whitespace
+              : ' '}
+            <span role="img" aria-label="Pouce levé">
+              👍
+            </span>
+          </Title>
+          <Typography paragraph>
+            Pôle Emploi va recevoir et traiter les documents que vous nous avez
+            fait parvenir. Si vous rencontrez un problème ou si vous vous posez
+            des questions, vous pouvez joindre votre conseiller depuis votre
+            espace personnel.
+          </Typography>
+          <br />
+          <Typography paragraph>
+            Si vous souhaitez envoyer d'autres documents à Pôle Emploi, merci de
+            le faire depuis votre espace personnel.
+          </Typography>
+        </Fragment>
+      ) : (
+        <Fragment>
+          <Title variant="title">
+            Merci, vos données ont bien été enregistrées.
+          </Title>
+          <Typography paragraph>
+            Vous pourrez reprendre ultérieurement.
+          </Typography>
+        </Fragment>
+      )}
+    </StyledThanks>
+  )
+}
 
 Thanks.propTypes = {
   activeMonth: PropTypes.instanceOf(Date),

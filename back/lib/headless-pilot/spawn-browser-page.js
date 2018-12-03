b560a94337879467cd17396b0572726e25cd4f48
Hugo Agbonon <hugo@agbonon.fr>
2018-12-03 18:11:58 +0100

    hotfix: change headless-pilot user-agent to avoid blocking

--- back/lib/headless-pilot/spawn-browser-page.js
+++ back/lib/headless-pilot/spawn-browser-page.js
@@ -5,7 +5,10 @@ module.exports = async () => {
     browserWSEndpoint: 'ws://browserless:3000',
   })
   const page = await browser.newPage()
-
+  // remove "HeadlessChrome" from the user agent to avoid being blocked
+  await page.setUserAgent(
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3466.0 Safari/537.36',
+  )
   // Try to prevent PDF preview from loading (causes crashes)
   await page.setRequestInterception(true)
   page.on('request', (interceptedRequest) => {

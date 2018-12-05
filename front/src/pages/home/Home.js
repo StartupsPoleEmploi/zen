c8ce11984265147c9b82d14269854f319cfbfaf2
Hugo Agbonon <hugo@agbonon.fr>
2018-12-05 10:29:44 +0100

    hotfix: show error modal to explain login outage

--- front/src/pages/home/Home.js
+++ front/src/pages/home/Home.js
@@ -1,19 +1,20 @@
 import Typography from '@material-ui/core/Typography'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
 
+import LoginErrorDialog from '../../components/Actu/LoginErrorDialog'
 import AppTitle from '../../components/Generic/AppTitle'
 import LinkButton from '../../components/Generic/LinkButton'
 import Rectangle from '../../components/Generic/Rectangle'
 import PEConnectLink from '../../components/PEConnect/PEConnectLink'
 import landingBackground from '../../images/landingBackground.svg'
 import logoPE from '../../images/logoPE.jpg'
-import step1ToStep2 from '../../images/step1-to-step2.svg'
-import step1 from '../../images/people.svg'
-import step2ToStep3 from '../../images/step2-to-step3.svg'
-import step2 from '../../images/paper-plane.svg'
 import step3 from '../../images/money-bank.svg'
+import step2 from '../../images/paper-plane.svg'
+import step1 from '../../images/people.svg'
+import step1ToStep2 from '../../images/step1-to-step2.svg'
+import step2ToStep3 from '../../images/step2-to-step3.svg'
 
 const StyledHome = styled.div`
   display: flex;
@@ -157,89 +158,112 @@ const FooterImg = styled.img`
   height: auto;
 `
 
-export const Home = ({ location: { search } }) => (
-  <StyledHome>
-    {search === '?loginFailed' && (
-      <LoginError>
-        <BodyTypography>
-          La connexion a échoué, merci de bien vouloir réessayer ultérieurement.
-        </BodyTypography>
-      </LoginError>
-    )}
-    <Header>
-      <HeaderMain>
-        <AppTitle />
-        <Rectangle
-          style={{
-            marginTop: '2rem',
-            marginBottom: '2rem',
-          }}
+export class Home extends Component {
+  static propTypes = {
+    location: PropTypes.shape({ search: PropTypes.string }),
+  }
+
+  state = { isErrorModalOpened: false }
+
+  componentDidMount() {
+    if (this.props.location.search === '?loginFailed') {
+      this.setState({ isErrorModalOpened: true }) // eslint-disable-line
+    }
+  }
+
+  closeModal = () => this.setState({ isErrorModalOpened: false })
+
+  render() {
+    const {
+      location: { search },
+    } = this.props
+    return (
+      <StyledHome>
+        {search === '?loginFailed' && (
+          <LoginError>
+            <BodyTypography>
+              La connexion a échoué, merci de bien vouloir réessayer
+              ultérieurement.
+            </BodyTypography>
+          </LoginError>
+        )}
+        <Header>
+          <HeaderMain>
+            <AppTitle />
+            <Rectangle
+              style={{
+                marginTop: '2rem',
+                marginBottom: '2rem',
+              }}
+            />
+            <Tagline>
+              Actualisez-vous<br />en toute simplicité
+            </Tagline>
+            <BodyTypography>
+              Vous avez plusieurs employeurs ?<br />
+              L’actualisation n’est pas claire ou compliquée{' '}?
+            </BodyTypography>
+            <BodyTypography style={{ marginTop: '3rem', marginBottom: '6rem' }}>
+              <b>
+                Avec Zen la mise à jour de votre situation et l’envoi de vos
+                documents sont plus clairs et simplifiés.
+              </b>
+            </BodyTypography>
+            <BigLinkButton to="/api/login" target="_self">
+              J'actualise ma situation
+            </BigLinkButton>
+            <Caption>
+              Propulsé par Pôle Emploi{' '}
+              <span role="img" aria-label="Fusée">
+                🚀
+              </span>
+            </Caption>
+          </HeaderMain>
+          <PEConnectLinkContainer>
+            <PEConnectLink useDarkVersion />
+          </PEConnectLinkContainer>
+        </Header>
+
+        <StepsContainer>
+          <Tagline>Seulement 3 étapes !</Tagline>
+          <Step>
+            <StepImg src={step1} alt="Étape 1" />
+            <StepTextContainer>
+              <StepTitle>Mise à jour de ma situation</StepTitle>
+              <StepText>
+                J’actualise ma situation en fonction de chaque employeur.
+              </StepText>
+            </StepTextContainer>
+          </Step>
+          <IntermediateImg src={step1ToStep2} alt="" />
+          <Step style={{ flexWrap: 'wrap-reverse' }}>
+            <StepTextContainer>
+              <StepTitle>Envoi simple et rapide</StepTitle>
+              <StepText>On me dit les documents que je dois envoyer.</StepText>
+            </StepTextContainer>
+            <StepImg src={step2} alt="Étape 2" />
+          </Step>
+          <IntermediateImg src={step2ToStep3} alt="" />
+          <Step>
+            <StepImg src={step3} alt="Étape 3" />
+            <StepTextContainer>
+              <StepTitle>Indemnisation sans erreur</StepTitle>
+              <StepText>Je suis payé le bon montant, au bon moment.</StepText>
+            </StepTextContainer>
+          </Step>
+        </StepsContainer>
+
+        <footer>
+          <FooterImg src={logoPE} alt="" />
+        </footer>
+
+        <LoginErrorDialog
+          isOpened={this.state.isErrorModalOpened}
+          onCancel={this.closeModal}
         />
-        <Tagline>
-          Actualisez-vous<br />en toute simplicité
-        </Tagline>
-        <BodyTypography>
-          Vous avez plusieurs employeurs ?<br />
-          L’actualisation n’est pas claire ou compliquée{' '}?
-        </BodyTypography>
-        <BodyTypography style={{ marginTop: '3rem', marginBottom: '6rem' }}>
-          <b>
-            Avec Zen la mise à jour de votre situation et l’envoi de vos
-            documents sont plus clairs et simplifiés.
-          </b>
-        </BodyTypography>
-        <BigLinkButton to="/api/login" target="_self">
-          J'actualise ma situation
-        </BigLinkButton>
-        <Caption>
-          Propulsé par Pôle Emploi{' '}
-          <span role="img" aria-label="Fusée">
-            🚀
-          </span>
-        </Caption>
-      </HeaderMain>
-      <PEConnectLinkContainer>
-        <PEConnectLink useDarkVersion />
-      </PEConnectLinkContainer>
-    </Header>
-
-    <StepsContainer>
-      <Tagline>Seulement 3 étapes !</Tagline>
-      <Step>
-        <StepImg src={step1} alt="Étape 1" />
-        <StepTextContainer>
-          <StepTitle>Mise à jour de ma situation</StepTitle>
-          <StepText>
-            J’actualise ma situation en fonction de chaque employeur.
-          </StepText>
-        </StepTextContainer>
-      </Step>
-      <IntermediateImg src={step1ToStep2} alt="" />
-      <Step style={{ flexWrap: 'wrap-reverse' }}>
-        <StepTextContainer>
-          <StepTitle>Envoi simple et rapide</StepTitle>
-          <StepText>On me dit les documents que je dois envoyer.</StepText>
-        </StepTextContainer>
-        <StepImg src={step2} alt="Étape 2" />
-      </Step>
-      <IntermediateImg src={step2ToStep3} alt="" />
-      <Step>
-        <StepImg src={step3} alt="Étape 3" />
-        <StepTextContainer>
-          <StepTitle>Indemnisation sans erreur</StepTitle>
-          <StepText>Je suis payé le bon montant, au bon moment.</StepText>
-        </StepTextContainer>
-      </Step>
-    </StepsContainer>
-
-    <footer>
-      <FooterImg src={logoPE} alt="" />
-    </footer>
-  </StyledHome>
-)
-
-Home.propTypes = {
-  location: PropTypes.shape({ search: PropTypes.string }),
+      </StyledHome>
+    )
+  }
 }
 
 export default Home

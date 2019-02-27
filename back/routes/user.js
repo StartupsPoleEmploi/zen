2e11a5be6afb49a7e8b8026be69cc89fa9a006c5
Hugo Agbonon <hugo@agbonon.fr>
2019-02-27 12:03:16 +0100

    front: Restore email form on Signup

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const Raven = require('raven')
+const config = require('config')
 
 const isUserTokenValid = require('../lib/isUserTokenValid')
+const sendSubscriptionConfirmation = require('../lib/mailings/sendSubscriptionConfirmation')
+
+const User = require('../models/User')
 
 const router = express.Router()
 
@@ -14,5 +19,35 @@ router.get('/', (req, res) =>
     csrfToken: req.csrfToken(),
   }),
 )
+
+router.patch('/', (req, res, next) => {
+  // This is only intended to let an user add his email,
+  // not modify it.
+  if (!req.session.user || !req.session.user.id || req.session.user.email) {
+    return res.status(401).json('Unauthorized')
+  }
+
+  return User.query()
+    .findOne({ id: req.session.user.id })
+    .then((user) => {
+      if (!user) throw new Error('No such user')
+
+      // We do not allow overriding previously obtained info.
+      return user.$query().patchAndFetch({
+        email: req.body.email,
+      })
+    })
+    .then((user) => {
+      if (config.get('shouldSendTransactionalEmails')) {
+        // Note: We do not wait for Mailjet to answer to send data back to the user
+        sendSubscriptionConfirmation(user).catch((e) =>
+          Raven.captureException(e),
+        )
+      }
+      req.session.user.email = user.email
+      res.json(req.session.user)
+    })
+    .catch(next)
+})
 
 module.exports = router

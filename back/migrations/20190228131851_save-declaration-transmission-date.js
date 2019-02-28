5e0a202846f5d356eb26cfb51f7907aeff32acf5
Hugo Agbonon <hugo@agbonon.fr>
2019-02-28 14:01:33 +0100

    db: add migration to add and populate declaration.transmittedAt column

---
+++ back/migrations/20190228131851_save-declaration-transmission-date.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable guard-for-in */
+
+exports.up = function(knex) {
+  return knex.schema
+    .table('declarations', (table) => {
+      table.timestamp('transmittedAt')
+      table.dropColumn('isTransmitted')
+    })
+    .then(() =>
+      knex.raw(`SELECT * FROM activity_logs where action='VALIDATE_EMPLOYERS'`),
+    )
+    .then(async ({ rows: logs }) => {
+      for (const i in logs) {
+        if (!logs[i].metadata.declarationId) continue // eslint-disable-line
+
+        await knex.raw(
+          `UPDATE declarations set "transmittedAt"='${logs[
+            i
+          ].createdAt.toISOString()}' WHERE id=${
+            logs[i].metadata.declarationId
+          }`,
+        )
+      }
+    })
+}
+
+exports.down = function(knex) {
+  return knex.schema.table('declarations', (table) => {
+    table.dropColumn('transmittedAt')
+    table.boolean('isTransmitted').defaultTo(false)
+  })
+}

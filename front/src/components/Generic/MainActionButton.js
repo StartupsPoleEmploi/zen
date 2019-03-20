c13676bb8024c3de59824ccf0aa4edee77c12018
Hugo Agbonon <hugo@agbonon.fr>
2019-03-20 14:59:27 +0100

    front: refactor action buttons into MainActionButton component

---
+++ front/src/components/Generic/MainActionButton.js
@@ -0,0 +1,53 @@
+import Button from '@material-ui/core/Button'
+import PropTypes from 'prop-types'
+import React from 'react'
+import styled from 'styled-components'
+
+const BaseButton = styled(Button).attrs({
+  color: 'primary',
+})`
+  && {
+    padding: 1rem 3rem;
+    margin: 0 0.5rem;
+    width: 17.5rem;
+    height: 6.5rem;
+  }
+`
+
+const PrimaryButton = styled(BaseButton).attrs({
+  variant: 'contained',
+})`
+  && {
+    &:disabled {
+      color: #fff;
+      background-color: rgba(
+        57,
+        103,
+        158,
+        0.5
+      ); /* rgb(57,103,158) is our primary color, #39679E */
+    }
+  }
+`
+
+const SecondaryButton = styled(BaseButton).attrs({
+  variant: 'outlined',
+})`
+  && {
+    color: #000;
+  }
+`
+
+const MainActionButton = ({ children, primary = true, ...props }) =>
+  primary ? (
+    <PrimaryButton {...props}>{children}</PrimaryButton>
+  ) : (
+    <SecondaryButton {...props}>{children}</SecondaryButton>
+  )
+
+MainActionButton.propTypes = {
+  children: PropTypes.node.isRequired,
+  primary: PropTypes.bool.isRequired,
+}
+
+export default MainActionButton

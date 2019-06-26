85d2275e53d8539e655bfd6ba6e6f67cfb5b83c1
Hugo Agbonon <hugo@agbonon.fr>
2019-06-26 11:21:17 +0200

    front: Create new vertical nav for Zen

--- front/src/pages/Layout.js
+++ front/src/pages/Layout.js
@@ -3,15 +3,24 @@ import ClickAwayListener from '@material-ui/core/ClickAwayListener'
 import { withStyles } from '@material-ui/core/styles'
 import Tooltip from '@material-ui/core/Tooltip'
 import Typography from '@material-ui/core/Typography'
+import File from '@material-ui/icons/Description'
 import ExpandLess from '@material-ui/icons/ExpandLess'
 import ExpandMore from '@material-ui/icons/ExpandMore'
+import FormatListBulleted from '@material-ui/icons/FormatListBulleted'
+import ArrowDown from '@material-ui/icons/KeyboardArrowDown'
+import ArrowUp from '@material-ui/icons/KeyboardArrowUp'
 import Person from '@material-ui/icons/PersonOutline'
+import { get } from 'lodash'
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
-import { withRouter } from 'react-router-dom'
+import React, { Component, Fragment } from 'react'
+import { Link, withRouter } from 'react-router-dom'
 import styled from 'styled-components'
 
 import AppTitle from '../components/Generic/AppTitle'
+import { primaryBlue } from '../constants/colors'
+
+const stepperRoutes = ['/actu', '/employers', '/files']
+const [declarationRoute, employersRoute, filesRoute] = stepperRoutes
 
 const styles = (theme) => ({
   lightTooltip: {
@@ -29,10 +38,26 @@ const StyledLayout = styled.div`
 
 const Header = styled.header`
   display: flex;
-  justify-content: space-around;
-  align-items: center;
-  flex: 1;
-  padding-left: 1.5rem;
+  justify-content: flex-end;
+`
+
+const UserButton = styled(Button)`
+  && {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: ${primaryBlue};
+    padding: 1.5rem 10rem;
+    border-radius: 0;
+    border-bottom-left-radius: 7rem 5rem;
+    color: #fff;
+    height: 5.5rem;
+    margin-bottom: -5.5rem;
+
+    &:hover {
+      background-color: ${primaryBlue};
+    }
+  }
 `
 
 const PersonIcon = styled(Person)`
@@ -42,15 +67,83 @@ const PersonIcon = styled(Person)`
   }
 `
 
-const Main = styled.main`
-  padding: 5rem 1rem;
+const Container = styled.div`
+  display: flex;
+  width: 100%;
 `
 
-const UserContainer = styled.div`
+const Main = styled.main`
+  padding: 7rem 1rem;
+  flex-grow: 1;
+`
+
+const Nav = styled.nav`
+  flex-shrink: 0;
+  width: 25rem;
+  border-right: 1px #ddd solid;
+  height: 100vh;
+`
+
+const AppTitleContainer = styled.div`
+  text-align: center;
+  border-bottom: 1px #ddd solid;
+  padding: 2rem;
+`
+
+const UlStepper = styled.ul`
   display: flex;
-  flex: 1;
+  flex-direction: column;
+  padding-left: 0;
+  padding-top: 2rem;
+`
+
+const LiStep = styled(Typography).attrs({ component: 'li' })`
+  && {
+    display: flex;
+    align-items: center;
+    padding-left: 5rem;
+    line-height: 4rem;
+    border-left: #fff 0.5rem solid;
+
+    &&.Stepper__Active {
+      border-left: ${primaryBlue} 0.5rem solid;
+    }
+  }
+`
+
+const StyledLink = styled(Link)`
+  display: flex;
   align-items: center;
-  justify-content: center;
+  color: ${primaryBlue};
+  text-decoration: none;
+  font-weight: bold;
+
+  &:visited {
+    color: #000;
+  }
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  &&.Stepper__Active__Link {
+    color: ${primaryBlue};
+  }
+`
+
+const Placeholder = styled.span`
+  display: inline-block;
+  width: 3.5rem;
+`
+
+const FileIcon = styled(File)`
+  width: 2.5rem;
+  margin-right: 1rem;
+`
+
+const ListIcon = styled(FormatListBulleted)`
+  width: 2.5rem;
+  margin-right: 1rem;
 `
 
 export class Layout extends Component {
@@ -63,10 +156,35 @@ export class Layout extends Component {
       email: PropTypes.string,
       csrfToken: PropTypes.string,
     }),
-    stepper: PropTypes.node,
+    history: PropTypes.shape({
+      replace: PropTypes.func.isRequired,
+    }).isRequired,
+    location: PropTypes.shape({ pathname: PropTypes.string.isRequired })
+      .isRequired,
+    activeMonth: PropTypes.instanceOf(Date),
+    activeDeclaration: PropTypes.object,
   }
 
   state = { isTooltipOpened: false }
+
+  getStepperItem = ({ label, link, shouldActivateLink, isActive }) => {
+    if (shouldActivateLink) {
+      return (
+        <LiStep className={isActive ? 'Stepper__Active' : ''}>
+          <StyledLink
+            to={link}
+            className={isActive ? 'Stepper__Active__Link' : ''}
+          >
+            {label}
+          </StyledLink>
+        </LiStep>
+      )
+    }
+
+    return (
+      <LiStep className={isActive ? 'Stepper__Active' : ''}>{label}</LiStep>
+    )
+  }
 
   setTooltipClosed = () => this.setState({ isTooltipOpened: false })
 
@@ -74,51 +192,131 @@ export class Layout extends Component {
     this.setState((state) => ({ isTooltipOpened: !state.isTooltipOpened }))
 
   render() {
-    const { children, classes, user, stepper } = this.props
+    const {
+      activeMonth,
+      activeDeclaration,
+      children,
+      classes,
+      user,
+      location: { pathname },
+    } = this.props
+
+    const userCanDeclare =
+      !get(user, 'hasAlreadySentDeclaration') && get(user, 'canSendDeclaration')
+
+    const shouldActivateDeclarationLink =
+      !!activeMonth &&
+      (!activeDeclaration ||
+        !activeDeclaration.hasFinishedDeclaringEmployers) &&
+      userCanDeclare
+
+    const shouldActivateEmployersLink =
+      !!activeMonth &&
+      !!activeDeclaration &&
+      !activeDeclaration.hasFinishedDeclaringEmployers &&
+      userCanDeclare
 
     return (
       <StyledLayout>
         <Header>
-          <AppTitle style={{ flex: 1, textAlign: 'center' }} />
-
-          {stepper}
-
+          {false && <AppTitle />}
           {user && (
-            <UserContainer>
-              <ClickAwayListener onClickAway={this.setTooltipClosed}>
-                <Tooltip
-                  classes={{ tooltip: classes.lightTooltip }}
-                  disableHoverListener
-                  disableFocusListener
-                  disableTouchListener
-                  open={this.state.isTooltipOpened}
-                  placement="bottom"
-                  title={
-                    <Button
-                      href="/api/login/logout"
-                      target="_self"
-                      disableRipple
-                      variant="text"
-                    >
-                      Déconnexion
-                    </Button>
-                  }
-                >
-                  <Button onClick={this.toggleTooltip} disableRipple>
-                    <PersonIcon />
-                    <Typography color="inherit">{user.firstName}</Typography>
-                    {this.state.isTooltipOpened ? (
-                      <ExpandLess />
-                    ) : (
-                      <ExpandMore />
-                    )}
+            <ClickAwayListener onClickAway={this.setTooltipClosed}>
+              <Tooltip
+                classes={{ tooltip: classes.lightTooltip }}
+                disableHoverListener
+                disableFocusListener
+                disableTouchListener
+                open={this.state.isTooltipOpened}
+                placement="bottom"
+                title={
+                  <Button
+                    href="/api/login/logout"
+                    target="_self"
+                    disableRipple
+                    variant="text"
+                  >
+                    Déconnexion
                   </Button>
-                </Tooltip>
-              </ClickAwayListener>
-            </UserContainer>
+                }
+              >
+                <UserButton onClick={this.toggleTooltip} disableRipple>
+                  <PersonIcon />
+                  {user.firstName}
+                  {this.state.isTooltipOpened ? <ExpandLess /> : <ExpandMore />}
+                </UserButton>
+              </Tooltip>
+            </ClickAwayListener>
           )}
         </Header>
-        <Main>{children}</Main>
+        <Container>
+          <div>
+            <Nav>
+              <AppTitleContainer>
+                <AppTitle />
+              </AppTitleContainer>
+              <UlStepper>
+                {this.getStepperItem({
+                  label: (
+                    <Fragment>
+                      <ListIcon /> Mon actualisation{' '}
+                      {activeMonth &&
+                        (pathname === declarationRoute ||
+                        pathname === employersRoute ? (
+                          <ArrowDown />
+                        ) : (
+                          <ArrowUp />
+                        ))}
+                    </Fragment>
+                  ),
+                  link: declarationRoute,
+                  isActive:
+                    pathname === declarationRoute ||
+                    pathname === employersRoute,
+                  shouldActivateLink:
+                    shouldActivateDeclarationLink ||
+                    shouldActivateEmployersLink,
+                })}
+                {(pathname === declarationRoute ||
+                  pathname === employersRoute) && (
+                  <Fragment>
+                    {this.getStepperItem({
+                      label: (
+                        <Fragment>
+                          <Placeholder /> Ma situation
+                        </Fragment>
+                      ),
+                      link: declarationRoute,
+                      shouldActivateLink: shouldActivateDeclarationLink,
+                      isActive: pathname === declarationRoute,
+                    })}
+                    {this.getStepperItem({
+                      label: (
+                        <Fragment>
+                          <Placeholder /> Mes employeurs
+                        </Fragment>
+                      ),
+                      link: employersRoute,
+                      shouldActivateLink: shouldActivateEmployersLink,
+                      isActive: pathname === employersRoute,
+                    })}
+                  </Fragment>
+                )}
+                {this.getStepperItem({
+                  label: (
+                    <Fragment>
+                      <FileIcon /> Mes justificatifs
+                    </Fragment>
+                  ),
+                  link: filesRoute,
+                  shouldActivateLink: true,
+                  isActive: pathname === filesRoute,
+                })}
+              </UlStepper>
+            </Nav>
+          </div>
+          <Main>{children}</Main>
+        </Container>
       </StyledLayout>
     )
   }

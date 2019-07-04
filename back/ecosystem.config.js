d828fe84414783ec6c2a176cc0bc068ab65779cb
Alexandre CANTIN <cantin.alexandre@gmail.com>
2019-07-04 09:32:00 +0200

    devEnv: no project reload when a PDF is generated

--- back/ecosystem.config.js
+++ back/ecosystem.config.js
@@ -6,12 +6,14 @@ const config = {
       watch:
         process.env.NODE_ENV === 'development' ||
         process.env.NODE_ENV === 'test',
+      ignore_watch: ['uploads'],
     },
     {
       name: 'admin',
       script: 'bin/www',
       args: '--admin',
       watch: process.env.NODE_ENV === 'development',
+      ignore_watch: ['uploads'],
     },
   ],
 }

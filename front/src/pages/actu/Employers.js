0a6c83b61f1d17087ad9427e888278b85affc67d
Hugo Agbonon <hugo@agbonon.fr>
2019-07-25 11:07:39 +0200

    front: Display EmployerQuestions fields full-width and vertical when on mobile

--- front/src/pages/actu/Employers.js
+++ front/src/pages/actu/Employers.js
@@ -1,6 +1,7 @@
 import Button from '@material-ui/core/Button'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import Typography from '@material-ui/core/Typography'
+import withWidth from '@material-ui/core/withWidth'
 import {
   isNaN as _isNaN,
   cloneDeep,
@@ -24,14 +25,14 @@ import PreviousEmployersDialog from '../../components/Actu/PreviousEmployersDial
 import WorkSummary from '../../components/Actu/WorkSummary'
 import AlwaysVisibleContainer from '../../components/Generic/AlwaysVisibleContainer'
 import MainActionButton from '../../components/Generic/MainActionButton'
-
+import { muiBreakpoints } from '../../constants'
 import {
-  WORK_HOURS,
-  SALARY,
-  MIN_SALARY,
-  MIN_WORK_HOURS,
   MAX_SALARY,
   MAX_WORK_HOURS,
+  MIN_SALARY,
+  MIN_WORK_HOURS,
+  SALARY,
+  WORK_HOURS,
 } from '../../lib/salary'
 
 const StyledEmployers = styled.div`
@@ -175,6 +176,7 @@ export class Employers extends Component {
       replace: PropTypes.func.isRequired,
     }).isRequired,
     token: PropTypes.string.isRequired,
+    width: PropTypes.string,
   }
 
   state = {
@@ -423,6 +425,7 @@ export class Employers extends Component {
       onChange={this.onChange}
       onRemove={this.onRemove}
       activeMonth={this.props.activeMonth}
+      verticalLayout={this.props.width === muiBreakpoints.xs}
     />
   )
 
@@ -504,4 +507,4 @@ export class Employers extends Component {
   }
 }
 
-export default Employers
+export default withWidth()(Employers)

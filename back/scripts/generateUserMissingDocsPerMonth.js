5bef507f97380d96bb34094f79b20e900ef2c18b
Hugo Agbonon <hugo@agbonon.fr>
2019-08-28 09:11:23 +0200

    back: add scripts to generate missing and untransmitted documents

---
+++ back/scripts/generateUserMissingDocsPerMonth.js
@@ -0,0 +1,111 @@
+/* eslint-disable no-await-in-loop */
+const { format, subDays } = require('date-fns')
+const { Parser } = require('json2csv')
+const fr = require('date-fns/locale/fr')
+const fs = require('fs')
+require('../lib/db')
+const Declaration = require('../models/Declaration')
+const EmployerDocument = require('../models/EmployerDocument')
+
+const documentLabels = {
+  sickLeave: 'Feuille maladie',
+  internship: 'Attestation de stage',
+  maternityLeave: 'Attestation de congé maternité',
+  retirement: 'Attestation retraite',
+  invalidity: 'Attestation invalidité',
+  employerCertificate: 'Attestation employeur',
+  salarySheet: 'Bulletin de salaire',
+}
+
+const getFormattedMonthAndYear = (date) =>
+  format(date, 'MMMM YYYY', { locale: fr })
+
+const getMissingDocumentLabelsFromDeclaration = (declaration) =>
+  declaration.infos
+    .filter(({ isTransmitted, file }) => !isTransmitted && !file)
+    .map(
+      ({ type }) =>
+        `${documentLabels[type]} / ${getFormattedMonthAndYear(
+          declaration.declarationMonth.month,
+        )}`,
+    )
+    .concat(
+      declaration.employers.reduce((declarationPrev, employer) => {
+        if (
+          employer.hasEndedThisMonth &&
+          !employer.documents.some(
+            (doc) => doc.type === EmployerDocument.types.employerCertificate,
+          )
+        ) {
+          return declarationPrev.concat(
+            `${documentLabels.employerCertificate} ${
+              employer.employerName
+            } / ${getFormattedMonthAndYear(
+              declaration.declarationMonth.month,
+            )}`,
+          )
+        }
+        if (employer.documents.length === 0) {
+          return declarationPrev.concat(
+            `${documentLabels.salarySheet} ${
+              employer.employerName
+            } / ${getFormattedMonthAndYear(
+              declaration.declarationMonth.month,
+            )}`,
+          )
+        }
+        return declarationPrev
+      }, []),
+    )
+
+for (let i = 1; i < 16; i++) {
+  Promise.all([
+    // Get unfinished declarations from users who have not received a reminder in the last day
+    Declaration.query()
+      .eager('[declarationMonth, infos, user, employers.documents]')
+      .join('Users', 'Users.id', '=', 'declarations.userId')
+      .where({
+        isFinished: false,
+        hasFinishedDeclaringEmployers: true,
+        monthId: i,
+      })
+      .andWhere(function() {
+        this.where(
+          'Users.lastDocsReminderDate',
+          '<',
+          subDays(new Date(), 1),
+        ).orWhereNull('Users.lastDocsReminderDate')
+      }),
+  ])
+    .then(([declarations]) => {
+      const monthLabel = format(
+        declarations[0].declarationMonth.month,
+        'MM-YYYY',
+      )
+
+      const csv = declarations.map((declaration) => {
+        const documents = getMissingDocumentLabelsFromDeclaration(declaration)
+
+        if (documents.length === 0) {
+          documents.push('Pas de document manquant')
+        }
+
+        return {
+          firstName: declaration.user.firstName,
+          lastName: declaration.user.lastName,
+          documents,
+        }
+      })
+
+      const fields = ['firstName', 'lastName', 'documents']
+
+      const json2csvParser = new Parser({ fields })
+      fs.writeFileSync(
+        `/home/back/scripts/${monthLabel}-documents-not-sent-to-zen.csv`,
+        json2csvParser.parse(csv),
+      )
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+}

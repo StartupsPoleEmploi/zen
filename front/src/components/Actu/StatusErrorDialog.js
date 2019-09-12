3a22418b426a4b2d09c5f9b9d9c4c32634c17770
Alexandre CANTIN <cantin.alexandre@gmail.com>
2019-09-12 15:21:04 +0200

    front: update message when PE Connect is down

--- front/src/components/Actu/StatusErrorDialog.js
+++ front/src/components/Actu/StatusErrorDialog.js
@@ -8,17 +8,31 @@ const StatusErrorDialog = ({ isOpened }) => (
   <CustomDialog
     content={
       <Fragment>
+        {/*
+          <DialogContentText style={{ marginBottom: '1rem' }}>
+            Suite à un problème indépendant de notre volonté, Zen est actuellement
+            indisponible.
+          </DialogContentText>
+          <DialogContentText style={{ marginBottom: '1rem' }}>
+            En cas d'urgence, vous pouvez effectuer vos opérations sur
+            Pole-Emploi.fr
+          </DialogContentText>
+          <DialogContentText style={{ marginBottom: '1rem' }}>
+            N'hésitez pas à nous contacter si vous avez besoin d'assistance. Nous
+            vous remercions de votre compréhension.
+          </DialogContentText>
+        */}
+
+        {/* TODO: remove after the 21/09/2019  */}
         <DialogContentText style={{ marginBottom: '1rem' }}>
-          Suite à un problème indépendant de notre volonté, Zen est actuellement
-          indisponible.
+          Bonjour,
         </DialogContentText>
         <DialogContentText style={{ marginBottom: '1rem' }}>
-          En cas d'urgence, vous pouvez effectuer vos opérations sur
-          Pole-Emploi.fr
+          Pour des raisons techniques, le service sera indisponible du vendredi
+          20 Septembre 20h au Samedi 21 Septembre.
         </DialogContentText>
         <DialogContentText style={{ marginBottom: '1rem' }}>
-          N'hésitez pas à nous contacter si vous avez besoin d'assistance. Nous
-          vous remercions de votre compréhension.
+          Nous nous excusons pour la gêne occasionnée.
         </DialogContentText>
       </Fragment>
     }

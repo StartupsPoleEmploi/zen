2ac7e04d6cce7989e86599a337f40595dc83ffed
Hugo Agbonon <hugo@agbonon.fr>
2019-10-25 15:10:19 +0200

    back: prevent systematic refreshToken on GET /user

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const Raven = require('raven')
 const config = require('config')
-const winston = require('../lib/log')
 
-const { isUserTokenValid, refreshToken } = require('../lib/token')
+const { isUserTokenValid } = require('../lib/token')
 const { refreshAccessToken } = require('../lib/refreshAccessTokenMiddleware')
 
 const sendSubscriptionConfirmation = require('../lib/mailings/sendSubscriptionConfirmation')
@@ -12,27 +11,13 @@ const User = require('../models/User')
 
 const router = express.Router()
 
-router.get('/', refreshAccessToken, (req, res) => {
-  const userTokenValid = isUserTokenValid(req.session.user.tokenExpirationDate)
-
-  const data = {
+router.get('/', refreshAccessToken, (req, res) =>
+  res.json({
     ...req.session.user,
     csrfToken: req.csrfToken && req.csrfToken(),
-  }
-
-  if (userTokenValid) {
-    // Don't send tokenExpirationDate but only boolean in order to avoid problems
-    // with client-side out of sync dates
-    return res.json({ ...data, isTokenValid: true })
-  }
-
-  return refreshToken(req)
-    .then(() => res.json({ ...data, isTokenValid: true }))
-    .catch((err) => {
-      winston.error(err)
-      res.json({ ...data, isTokenValid: false })
-    })
-})
+    isTokenValid: isUserTokenValid(req.session.user.tokenExpirationDate),
+  }),
+)
 
 router.patch('/', (req, res, next) => {
   // This is only intended to let an user add his email,

b71395eab3ff2eb8978c1eac1fea68ae87025a7a
Alexan <cantin.alexandre@gmail.com>
2019-11-05 14:01:51 +0100

    back: add route for getting current and next month

--- back/routes/declarationMonths.js
+++ back/routes/declarationMonths.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const DeclarationMonth = require('../models/DeclarationMonth')
 
 const router = express.Router()
 
@@ -6,5 +7,30 @@ router.get('/', (req, res) => {
   if (!('active' in req.query)) return res.status(400).json('Route not ready')
   res.json((req.activeMonth && req.activeMonth.month) || null)
 })
+
+router.get('/next-declaration-month', (req, res) =>
+  DeclarationMonth.query()
+    .select(['month', 'startDate', 'endDate'])
+    .where('endDate', '>', 'now')
+    .andWhere('startDate', '>', 'now')
+    .orderBy('startDate')
+    .first()
+    .then((month) => {
+      if (!month) throw new Error('Next declaration month not found.')
+      return res.json(month)
+    }),
+)
+
+router.get('/current-declaration-month', (req, res) =>
+  DeclarationMonth.query()
+    .select(['month', 'startDate', 'endDate'])
+    .where('endDate', '>', 'now')
+    .andWhere('startDate', '<=', 'now')
+    .first()
+    .then((month) => {
+      if (!month) throw new Error('Current declaration month not found.')
+      return res.json(month)
+    }),
+)
 
 module.exports = router

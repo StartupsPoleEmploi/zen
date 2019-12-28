5050b09db206be48fc7114d98bcc39f3a653589e
Alexan <cantin.alexandre@gmail.com>
2019-12-28 08:35:50 +0100

    back: fix PDF content

--- back/lib/pdfGenerators/declarationProof/index.js
+++ back/lib/pdfGenerators/declarationProof/index.js
@@ -16,12 +16,12 @@ const readFile = util.promisify(fs.readFile)
 const writeFile = util.promisify(fs.writeFile)
 let templateHTML // For caching
 
-const documentLabels = {
-  sickLeave: 'arrêt maladie',
-  internship: 'stage',
-  maternityLeave: 'congé maternité',
-  retirement: 'retraite',
-  invalidity: 'invalidité',
+const DOCUMENT_LABELS = {
+  sickLeave: 'Avoir été en arrêt maladie',
+  internship: 'Avoir été en stage',
+  maternityLeave: 'Avoir été en congé maternité',
+  retirement: 'Percevoir une nouvelle pension retraite',
+  invalidity: 'Percevoir une nouvelle pension d’invalidité',
 }
 
 const formatIntervalDates = (startDate, endDate) => {
@@ -45,12 +45,21 @@ function getFriendlyPdfName({ declarationMonth: { month } }) {
 const generatePdfPath = (declaration) =>
   `${uploadsDeclarationDirectory}${generatePdfName(declaration)}`
 
+function lowerCaseFirstLetter(string) {
+  return string.charAt(0).toLowerCase() + string.slice(1)
+}
+
 const generateDeclarationAsPdf = async (declaration, pdfPath) => {
   // Generate actualisation/ folder if not exists
   const folderExists = await exists(uploadsDeclarationDirectory)
   if (!folderExists) await mkDir(uploadsDeclarationDirectory)
 
   const data = {
+    user: {
+      email: declaration.user.email.toLowerCase(),
+      firstName: declaration.user.firstName,
+      lastName: declaration.user.lastName.toUpperCase(),
+    },
     baseUrl: isProdEnv
       ? 'http://zen.pole-emploi.fr'
       : 'http://zen.beta.pole-emploi.fr',
@@ -76,15 +85,13 @@ const generateDeclarationAsPdf = async (declaration, pdfPath) => {
       (prev, { salary }) => salary + prev,
       0,
     ),
-    specialSituations: declaration.infos
-      .filter((info) => !['jobSearch'].includes(info.type))
-      .map(
-        (info) =>
-          `Avoir été en ${documentLabels[info.type]} ${formatIntervalDates(
-            info.startDate,
-            info.endDate,
-          )}`,
-      ),
+    specialSituations: Object.entries(DOCUMENT_LABELS).map(([key, val]) => {
+      const info = declaration.infos.find((e) => e.type === key)
+      if (info) {
+        return `${val} ${formatIntervalDates(info.startDate, info.endDate)}`
+      }
+      return `Ne pas ${lowerCaseFirstLetter(val)}`
+    }),
   }
 
   if (!templateHTML) templateHTML = await readFile(DECLARATION_PDF_TEMPLATE)

28259e7c7be652466c4370038732c7d82428f4bf
yalap_jeanclaude <yalap.jeanclaude@gmail.com>
2020-02-10 17:32:57 +0100

    [Back] update store time from 1 min to 5 sec

--- back/lib/serviceUpMiddleware.js
+++ back/lib/serviceUpMiddleware.js
@@ -1,14 +1,14 @@
-const { subMinutes } = require('date-fns')
+const { subSeconds } = require('date-fns')
 const Status = require('../models/Status')
 
 let serviceUp = null
 let filesServiceUp = null
 let valueStoreDate = new Date(0)
 
-// Only request isServiceUp once every 1 minute.
+// Only request isServiceUp once every 5 seconds.
 // set it in req.isServiceUp
 const setIsServiceUp = (req, res, next) => {
-  if (valueStoreDate > subMinutes(new Date(), 1)) {
+  if (valueStoreDate > subSeconds(new Date(), 5)) {
     req.isServiceUp = serviceUp
     req.isFilesServiceUp = filesServiceUp
     return next()

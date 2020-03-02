a7e200035c9c62202f5a7a16a571c690d562d0db
yalap_jeanclaude <yalap.jeanclaude@gmail.com>
2020-03-02 17:37:46 +0100

    [Back] remove winston-elasticsearch

--- back/lib/log.js
+++ back/lib/log.js
@@ -1,16 +1,9 @@
 const winston = require('winston')
 const SlackHook = require("winston-slack-webhook-transport");
-const Elasticsearch = require('winston-elasticsearch');
 
 const {
   WINSTON_ENABLE_SLACK,
   SLACK_WEBHOOK_SU_ZEN_TECH,
-  WINSTON_ENABLE_ELASTICSEARCH,
-  ELASTICSEARCH_LOG_INDEX,
-  ELASTICSEARCH_LOG_INDEX_PREFIX,
-  ELASTICSEARCH_LOG_URL,
-  ELASTICSEARCH_LOG_USER,
-  ELASTICSEARCH_LOG_PWD,
 } = process.env;
 
 if (WINSTON_ENABLE_SLACK === 'true') {
@@ -20,21 +13,6 @@ if (WINSTON_ENABLE_SLACK === 'true') {
     formatter: ({ level, message }) => ({
       text: `*${level}*: ${message}`,
     }),
-  }))
-}
-
-if (WINSTON_ENABLE_ELASTICSEARCH) {
-  winston.add(new Elasticsearch({
-    level: 'debug',
-    index: ELASTICSEARCH_LOG_INDEX,
-    indexPrefix: ELASTICSEARCH_LOG_INDEX_PREFIX,
-    clientOpts: {
-      node: ELASTICSEARCH_LOG_URL,
-      auth: {
-        username: ELASTICSEARCH_LOG_USER,
-        password: ELASTICSEARCH_LOG_PWD
-      }
-    }
   }))
 }
 

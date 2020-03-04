91561d3f31d8b5ee35d5dd8d54136c1c82a1b420
yalap_jeanclaude <yalap.jeanclaude@gmail.com>
2020-03-04 11:34:06 +0100

    [Back] Add file log

--- back/lib/log.js
+++ back/lib/log.js
@@ -3,8 +3,33 @@ const SlackHook = require("winston-slack-webhook-transport");
 
 const {
   WINSTON_ENABLE_SLACK,
+  WINSTON_ENABLE_LOG,
+  WINSTON_ENABLE_FILE,
   SLACK_WEBHOOK_SU_ZEN_TECH,
+  WINSTON_FILE_FOLDER,
 } = process.env;
+
+if (WINSTON_ENABLE_LOG === 'true') {
+  winston.add(new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.colorize(), 
+      winston.format.printf(
+        ({level, message, ...meta}) => `${level}: ${JSON.stringify(message, null, 4)} ${JSON.stringify(meta)}\n`,
+      ),
+    )
+  }))
+}
+
+if (WINSTON_ENABLE_FILE === 'true') {
+  winston.add(new winston.transports.File({
+    level: 'error',
+    filename: `${WINSTON_FILE_FOLDER}/error.log`,
+  }))
+  winston.add(new winston.transports.File({
+    level: 'debug',
+    filename: `${WINSTON_FILE_FOLDER}/all.log`,
+  }))
+}
 
 if (WINSTON_ENABLE_SLACK === 'true') {
   winston.add(new SlackHook({

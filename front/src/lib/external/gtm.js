e1f48c6b7b17896803819643e2f2ffceaec17d42
François-Xavier Montigny <francoisxavier.montigny@ysocorp.com>
2020-07-03 11:50:05 +0200

    change google id

--- front/src/lib/external/gtm.js
+++ front/src/lib/external/gtm.js
@@ -11,4 +11,4 @@
   j.async = true
   j.src = `https://www.googletagmanager.com/gtm.js?id=${i}${dl}`
   f.parentNode.insertBefore(j, f)
-})(window, document, 'script', 'dataLayer', 'GTM-58WMQPJ')
+})(window, document, 'script', 'dataLayer', 'GTM-TQ4ZBQR')

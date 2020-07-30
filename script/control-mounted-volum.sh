96771987346fa8197b95f8838ad326468690015f
François-Xavier Montigny <francoisxavier.montigny@ysocorp.com>
2020-07-30 10:25:54 +0200

    add script to mounted upload folder

---
+++ script/control-mounted-volum.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+## 
+# This script check if the uploads folder are mounted
+##
+
+DOCKERNAME=zen_node_1
+MOUNTEDFOLDER=uploads
+
+TESTCMD=$(docker exec -it $DOCKERNAME ls $MOUNTEDFOLDER)
+echo "Check mounted status"
+if [[ $TESTCMD =~ ^ls:* ]] ; then
+  echo "$(date)"
+  echo "${TESTCMD}"
+  echo "Restarting docker $DOCKERNAME"
+  docker restart $DOCKERNAME    
+else
+    echo "Everything is fine!"
+fi
+
+
+# to executable
+# chmod +x script/control-mounted-volum.sh
+
+# add to cron
+# crontab -e
+# */5 * * * * mkdir -p /home/docker/zen/logs/ && /home/docker/zen/script/control-mounted-volum.sh > /home/docker/zen/logs/control-mounted-volum.log 2>&1
+

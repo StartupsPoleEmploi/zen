08ccdf07ddd45967b5e9e1e7f7f6f69f35b0e640
François-Xavier Montigny <francoisxavier.montigny@ysocorp.com>
2020-09-24 12:17:41 +0200

    add hotjar and GTM

--- front/src/index.js
+++ front/src/index.js
@@ -7,6 +7,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { GTM } from './lib/external/gtm';
+import './lib/external/hotjar';
 
 import { version } from '../package.json';
 import App from './App';
@@ -24,6 +26,10 @@ if (environment !== 'development') {
       environment,
     },
   ).install();
+}
+
+if (process.env.REACT_APP_GOOGLE_TAG_MANAGER_KEY) {
+  GTM(process.env.REACT_APP_GOOGLE_TAG_MANAGER_KEY);
 }
 
 moment.locale('fr');

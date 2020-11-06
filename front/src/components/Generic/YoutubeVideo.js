77c2bf69c40cf72a5db84d77908ce4779d64f3e6
François-Xavier Montigny <francoisxavier.montigny@ysocorp.com>
2020-11-06 11:03:28 +0100

    replace video confirmation

--- front/src/components/Generic/YoutubeVideo.js
+++ front/src/components/Generic/YoutubeVideo.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import ReactComment from './ReactComment';
+
+import image from '../../images/youtube-video-thumb.jpg';
+import player from '../../images/player.svg';
+
+const Button = styled.button`
+  border: none;
+  margin: 0;
+  padding: 0;
+  cursor: pointer;
+  background: none;
+  display: block;
+  position: relative;
+
+  background-position: center;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-color: #fff;
+
+  &::-moz-focus-inner {
+    border: 0;
+    padding: 0;
+  }
+`;
 
 const style = {
   width: '62rem',
@@ -14,35 +38,72 @@ const shadowStyle = {
   boxShadow: '0 0 1.5rem 0.5rem rgba(0, 0, 0, 0.05)',
 };
 
-export function YoutubeVideo({ id }) {
-  return (
-    <div style={{ ...style, ...shadowStyle }} id={id} tag="1001">
-      <ReactComment text={`<iframe sandbox="allow-scripts allow-same-origin allow-presentation" 
-      title="Vidéo de présentation du service Zen" 
-      src="https://www.youtube.com/embed/lnOxn6yzMyw" 
-      frameborder="0" 
-      allow="encrypted-media" 
-      allowfullscreen="" 
-      style="width: 100%; height: 35rem;"></iframe>`}
-      />
-      {/* <iframe
-        sandbox="allow-scripts allow-same-origin allow-presentation"
-        title={title}
-        src="https://www.youtube.com/embed/lnOxn6yzMyw?autoplay=1&rel=0"
-        frameBorder="0"
-        allow="autoplay; encrypted-media"
-        allowFullScreen
-        style={{
-          width: '100%',
-          height: '35rem',
-        }}
-      /> */}
-    </div>
-  );
-}
+export class YoutubeVideo extends Component {
+  static propTypes = { id: PropTypes.string.isRequired }
 
-YoutubeVideo.propTypes = {
-  id: PropTypes.string.isRequired,
-};
+  state = {
+    showVideo: false,
+  }
+
+  showVideo = () => this.setState({ showVideo: true })
+
+  render() {
+    const title = 'Vidéo de présentation du service Zen';
+
+    if (this.state.showVideo) {
+      return (
+        <div style={{ ...style, ...shadowStyle }} id={this.props.id} tag="1001">
+          <ReactComment text={`<iframe sandbox="allow-scripts allow-same-origin allow-presentation" 
+          title="Vidéo de présentation du service Zen" 
+          src="https://www.youtube.com/embed/lnOxn6yzMyw?autoplay=1&amp;rel=0" 
+          frameborder="0" 
+          allow="autoplay; encrypted-media" 
+          allowfullscreen="" 
+          style="width: 100%; height: 35rem;"></iframe>`}
+          />
+          {/* <iframe
+            sandbox="allow-scripts allow-same-origin allow-presentation"
+            title={title}
+            src="https://www.youtube.com/embed/lnOxn6yzMyw?autoplay=1&rel=0"
+            frameBorder="0"
+            allow="autoplay; encrypted-media"
+            allowFullScreen
+            style={{
+              width: '100%',
+              height: '35rem',
+            }}
+          /> */}
+        </div>
+      );
+    }
+
+    return (
+      <Button
+        type="button"
+        title={`Visualiser : ${title}`}
+        onClick={this.showVideo}
+        style={{ ...style, ...shadowStyle }}
+        id={this.props.id}
+      >
+        <div
+          style={{
+            position: 'absolute',
+            display: 'flex',
+            top: 0,
+            left: 0,
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+            width: '100%',
+          }}
+          tag="1001"
+        >
+          <img src={player} alt="" />
+        </div>
+        <img src={image} alt={title} style={style} />
+      </Button>
+    );
+  }
+}
 
 export default YoutubeVideo;

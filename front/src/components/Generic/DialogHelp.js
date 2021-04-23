a81e39d80590076e75c29520f2ff4ff150576526
claire-anne bertrais <claire-anne.bertrais-ext@pole-emploi.fr>
2021-04-23 16:28:22 +0200

    (fix) FAQ

--- front/src/components/Generic/DialogHelp.js
+++ front/src/components/Generic/DialogHelp.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import styled from 'styled-components';
-import { Typography, Grid, Link } from '@material-ui/core';
+import { Typography, Grid } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import { connect } from 'react-redux';
 import MessageOutlined from '@material-ui/icons/MessageOutlined';

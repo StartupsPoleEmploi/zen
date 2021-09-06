17477c0a1c962998e7a9be4cdb64d7bd5441747c
Jérôme Devy <jerome.devy-ext@pole-emploi.fr>
2021-09-06 17:37:43 +0200

    fix(backup): modifie la sauvegarde des pdf pour optimiser la place

--- backups/BACKUP.sh
+++ backups/BACKUP.sh
@@ -5,7 +5,11 @@ WEEKDAY=`date +%A`;
 NICE="/usr/bin/nice -n 15";
 
 NAME=docker_$WEEKDAY.tar.bz2
+NAME_PDF=pdf.tar.bz2
 
 mkdir -p /mnt/backups/$BACKUPS_ENV
 
-$NICE tar --exclude=*.log --exclude=*.gz --exclude=*.bz2 --exclude=node_modules/* -jcPf - /home/docker | tee /mnt/backups/$BACKUPS_ENV/$NAME /home/backups/$NAME >/dev/null;
+$NICE tar --exclude=*.log --exclude=*.gz --exclude=*.bz2 --exclude=node_modules/* --exclude=*.pdf -jcPf - /home/docker | tee /mnt/backups/$BACKUPS_ENV/$NAME /home/backups/$NAME >/dev/null;
+
+$NICE tar -jcPf - /home/docker/back/uploads | tee /mnt/backups/$BACKUPS_ENV/$NAME_PDF /home/backups/$NAME_PDF >/dev/null;
+
